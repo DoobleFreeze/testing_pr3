b4d8475c03f51e6e31003ccd16997799fedfa74f
Осипов Н <timef900@yandex.ru>
2022-10-14 22:49:21 +0300

    Add create_board and success UnitTest

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
+import random
+
+
 class Board:
     def __init__(self, weight, height, count_bomb):
         self.weight = weight
         self.height = height
         self.count_bomb = count_bomb
-        self.board = [["0" for _ in range(self.weight)] for _ in range(self.height)]
-        self.user_board = [["*" for _ in range(self.weight)] for _ in range(self.height)]
+        self.board = [[0] * self.weight for _ in range(self.height)]
+        self.user_board = [["*"] * self.weight for _ in range(self.height)]
 
     def create_board(self):
         # TODO: Растановка бомб и чисел на поле
-        return None
+        count_bomb = self.count_bomb
+        while count_bomb != 0:
+            x = random.randint(0, self.weight - 1)
+            y = random.randint(0, self.height - 1)
+            if self.board[y][x] == 0:
+                self.board[y][x] = "b"
+                count_bomb -= 1
+        for j in range(self.height):
+            for i in range(self.weight):
+                if self.board[j][i] != "b":
+                    if i - 1 >= 0 and j - 1 >= 0 and self.board[j - 1][i - 1] == "b":
+                        self.board[j][i] += 1
+                    if j - 1 >= 0 and self.board[j - 1][i] == "b":
+                        self.board[j][i] += 1
+                    if i + 1 < self.weight and j - 1 >= 0 and self.board[j - 1][i + 1] == "b":
+                        self.board[j][i] += 1
+                    if i - 1 >= 0 and self.board[j][i - 1] == "b":
+                        self.board[j][i] += 1
+                    if i + 1 < self.weight and self.board[j][i + 1] == "b":
+                        self.board[j][i] += 1
+                    if i - 1 >= 0 and j + 1 < self.height and self.board[j + 1][i - 1] == "b":
+                        self.board[j][i] += 1
+                    if j + 1 < self.height and self.board[j + 1][i] == "b":
+                        self.board[j][i] += 1
+                    if i + 1 < self.weight and j + 1 < self.height and self.board[j + 1][i + 1] == "b":
+                        self.board[j][i] += 1
+        return self.user_board
+
+    def check_board(self):
+        return self.board
 
     def open_place(self, x, y):
         # TODO: Открытие клетки
--- testing_board.py
+++ testing_board.py
@@ -9,16 +9,31 @@ class TestBoard(unittest.TestCase):
         self.board_3_x_3 = Board(3, 3, 2)
 
     def test_board_5_x_5(self):
-        a = [["*" for _ in range(5)] for _ in range(5)]
+        a = [["*"] * 5 for _ in range(5)]
         self.assertEqual(self.board_5_x_5.create_board(), a)
+        b = self.board_5_x_5.check_board()
+        count_bomb = 0
+        for i in b:
+            count_bomb += i.count('b')
+        self.assertEqual(count_bomb, 5)
 
     def test_board_1_x_1(self):
-        a = [["*" for _ in range(1)] for _ in range(1)]
+        a = [["*"] * 1 for _ in range(1)]
         self.assertEqual(self.board_1_x_1.create_board(), a)
+        b = self.board_1_x_1.check_board()
+        count_bomb = 0
+        for i in b:
+            count_bomb += i.count('b')
+        self.assertEqual(count_bomb, 1)
 
     def test_board_3_x_3(self):
-        a = [["*" for _ in range(3)] for _ in range(3)]
+        a = [["*"] * 3 for _ in range(3)]
         self.assertEqual(self.board_3_x_3.create_board(), a)
+        b = self.board_3_x_3.check_board()
+        count_bomb = 0
+        for i in b:
+            count_bomb += i.count('b')
+        self.assertEqual(count_bomb, 2)
 
 
 if __name__ == "__main__":
