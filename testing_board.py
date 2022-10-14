168f8be797a2c55ad1c0c494f267e9d2dd4f96c8
Осипов Н <timef900@yandex.ru>
2022-10-14 23:00:12 +0300

    Add new UnitTest

--- testing_board.py
+++ testing_board.py
@@ -11,29 +11,56 @@ class TestBoard(unittest.TestCase):
     def test_board_5_x_5(self):
         a = [["*"] * 5 for _ in range(5)]
         self.assertEqual(self.board_5_x_5.create_board(), a)
+
         b = self.board_5_x_5.check_board()
         count_bomb = 0
         for i in b:
             count_bomb += i.count('b')
         self.assertEqual(count_bomb, 5)
 
+        c = a.copy()
+        c[0][0] = b[0][0]
+        if b[0][0] == 'b':
+            lose = True
+        else:
+            lose = False
+        self.assertEqual(self.board_5_x_5.open_place(0, 0), (c, lose))
+
     def test_board_1_x_1(self):
         a = [["*"] * 1 for _ in range(1)]
         self.assertEqual(self.board_1_x_1.create_board(), a)
+
         b = self.board_1_x_1.check_board()
         count_bomb = 0
         for i in b:
             count_bomb += i.count('b')
         self.assertEqual(count_bomb, 1)
 
+        c = a.copy()
+        c[0][0] = b[0][0]
+        if b[0][0] == 'b':
+            lose = True
+        else:
+            lose = False
+        self.assertEqual(self.board_1_x_1.open_place(0, 0), (c, lose))
+
     def test_board_3_x_3(self):
         a = [["*"] * 3 for _ in range(3)]
         self.assertEqual(self.board_3_x_3.create_board(), a)
+
         b = self.board_3_x_3.check_board()
         count_bomb = 0
         for i in b:
             count_bomb += i.count('b')
         self.assertEqual(count_bomb, 2)
+
+        c = a.copy()
+        c[0][0] = b[0][0]
+        if b[0][0] == 'b':
+            lose = True
+        else:
+            lose = False
+        self.assertEqual(self.board_3_x_3.open_place(0, 0), (c, lose))
 
 
 if __name__ == "__main__":
